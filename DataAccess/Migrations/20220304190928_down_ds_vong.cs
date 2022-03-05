33d04d7a0e627d76e698c4dea0d0d18933a6e288
Veronica Vu <ptv0002@uah.edu>
2022-03-04 21:16:53 -0600

    Background edit. Round tabs edit

---
+++ DataAccess/Migrations/20220304190928_down_ds_vong.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class down_ds_vong : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DS_Diems_DS_Vongs_ID_Vong",
+                table: "DS_Diems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_DS_Trans_DS_Trinhs_DS_TrinhId",
+                table: "DS_Trans");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_DS_Trans_DS_Vongs_ID_Vong",
+                table: "DS_Trans");
+
+            migrationBuilder.DropTable(
+                name: "DS_Vongs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DS_Trans_DS_TrinhId",
+                table: "DS_Trans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DS_Trans_ID_Vong",
+                table: "DS_Trans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DS_Diems_ID_Vong",
+                table: "DS_Diems");
+
+            migrationBuilder.DropColumn(
+                name: "DS_TrinhId",
+                table: "DS_Trans");
+
+            migrationBuilder.RenameColumn(
+                name: "ID_Vong",
+                table: "DS_Trans",
+                newName: "Ma_Vong");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DS_Trans_ID_Trinh",
+                table: "DS_Trans",
+                column: "ID_Trinh");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DS_Trans_DS_Trinhs_ID_Trinh",
+                table: "DS_Trans",
+                column: "ID_Trinh",
+                principalTable: "DS_Trinhs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DS_Trans_DS_Trinhs_ID_Trinh",
+                table: "DS_Trans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DS_Trans_ID_Trinh",
+                table: "DS_Trans");
+
+            migrationBuilder.RenameColumn(
+                name: "Ma_Vong",
+                table: "DS_Trans",
+                newName: "ID_Vong");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DS_TrinhId",
+                table: "DS_Trans",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "DS_Vongs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ma_Vong = table.Column<int>(type: "int", nullable: false),
+                    Ten = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DS_Vongs", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DS_Trans_DS_TrinhId",
+                table: "DS_Trans",
+                column: "DS_TrinhId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DS_Trans_ID_Vong",
+                table: "DS_Trans",
+                column: "ID_Vong");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DS_Diems_ID_Vong",
+                table: "DS_Diems",
+                column: "ID_Vong");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DS_Diems_DS_Vongs_ID_Vong",
+                table: "DS_Diems",
+                column: "ID_Vong",
+                principalTable: "DS_Vongs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DS_Trans_DS_Trinhs_DS_TrinhId",
+                table: "DS_Trans",
+                column: "DS_TrinhId",
+                principalTable: "DS_Trinhs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DS_Trans_DS_Vongs_ID_Vong",
+                table: "DS_Trans",
+                column: "ID_Vong",
+                principalTable: "DS_Vongs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Tennis_Web/Controllers/ResultController.cs
+++ Tennis_Web/Controllers/ResultController.cs
@@ -1,11 +1,14 @@
 ﻿using DataAccess;
 using Library;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Tennis_Web.Models;
 
@@ -14,48 +17,76 @@ namespace Tennis_Web.Controllers
     public class ResultController : Controller
     {
         private readonly TennisContext _context;
-        public ResultController(TennisContext context)
+        private readonly IWebHostEnvironment _webHost;
+        public ResultController(TennisContext context, IWebHostEnvironment webHost)
         {
             _context = context;
+            _webHost = webHost;
         }
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult UpdateTable(TableTabViewModel model)
+        public async Task<IActionResult> UpdateTableAsync(RoundTabViewModel model)
         {
             var matches = model.DS_Tran;
             var temp = _context.DS_Trinhs.Include(m => m.DS_Giai).FirstOrDefault(m => m.Id == matches.Find(m => m.ID_Trinh != 0).ID_Trinh);
             // Find and update result for Matches
             bool result = false;
-            var columnsToSave = new List<string> { "Kq_1", "Kq_2" };
             if (ModelState.IsValid)
             {
-                //bool tableFilled = matches.Where(m => m.DS_Vong.Ma_Vong == 7).All(m => (m.Kq_1 + m.Kq_2) > 0);
-                //bool tableChanges = !IsTheSame(matches.Where(m => m.DS_Vong.Ma_Vong == 7).ToList(), temp.Id);
-                //if (tableChanges)
+                var columnsToSave = new List<string> { "Kq_1", "Kq_2" };
+                bool tableFilled = matches.Where(m => m.Ma_Vong == 8).All(m => (m.Kq_1 + m.Kq_2) > 0);
+                bool tableChanges = !IsTheSame(matches.Where(m => m.Ma_Vong == 7).ToList(), temp.Id);
+                bool playoffEmpty = matches.Any(m => m.Ma_Vong == 7 && (m.ID_Cap1 == null || m.ID_Cap2 == null));
+
+                FileStream fileStream = System.IO.File.OpenRead(_webHost.WebRootPath + "/Files/Json/MatchGenParam.json");
+                var file = await JsonSerializer.DeserializeAsync<List<MatchGeneratorViewModel>>(fileStream);
+                fileStream.Dispose();
+                if (tableChanges)
+                {
+                    foreach (var match in matches.Where(m => m.Ma_Vong == 8).ToList())
+                    {
+                        result = new DatabaseMethod<DS_Tran>(_context).SaveObjectToDB(match.Id, match, columnsToSave).Succeeded;
+                        if (!result) break;
+                    }
+                    if (result) _context.SaveChanges();
+                    // ========================= Rank pairs =========================
+
+                    var tables = _context.DS_Bangs.Where(m => m.ID_Trinh == temp.Id);
+                    foreach (var table in tables)
+                    {
+                        var rankedPairs = new TennisMethod(_context).Rank_Full(temp.Id, table.Ten);
+                        foreach (var pair in rankedPairs)
+                        {
+                            result = new DatabaseMethod<DS_Cap>(_context).SaveObjectToDB(pair.Id, pair, new List<string> { "Xep_Hang", "Tran_Thang" }).Succeeded;
+                            if (!result) break;
+                        }
+                    }
+                    if (result) _context.SaveChanges();
+
+                }
+                //if (!playoffEmpty)
                 //{
-                //    update:
-                //    foreach (var match in matches)
+                //    foreach (var match in matches.Where(m => m.Ma_Vong == 7).ToList())
                 //    {
                 //        result = new DatabaseMethod<DS_Tran>(_context).SaveObjectToDB(match.Id, match, columnsToSave).Succeeded;
                 //        if (!result) break;
-                //    }
-                //    if (result) _context.SaveChanges();
-                //    if (tableFilled)
-                //    {
-                //        // ========================= Rank pairs here =========================
-
                 //    }
                 //}
-                //else
+                //foreach (var level in file)
                 //{
-                //    matches = matches.Where(m => m.DS_Vong.Ma_Vong == 8).ToList();
-                //    foreach (var match in matches)
+                //    if (level.PlayOff1 > 0)
+                //    if (level.PlayOff2 > 0)
                 //    {
-                //        result = new DatabaseMethod<DS_Tran>(_context).SaveObjectToDB(match.Id, match, columnsToSave).Succeeded;
-                //        if (!result) break;
-                //    }
+                //        if (level.ChosenPerTable.se)
+                //        }
+                //}
+                //bool uniqueRanking =
+                //    // ========================= Get ranking and put in playoff =========================
+                //    if (tableFilled && playoffEmpty)
+                //{
+
                 //}
                 // ================== Add players to Special rounds ==================
             }
