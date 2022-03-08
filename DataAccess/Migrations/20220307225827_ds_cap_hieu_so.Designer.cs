f3d36315a058ead3cfcfe3295e4d304036ad7818
Veronica Vu <ptv0002@uah.edu>
2022-03-07 18:33:44 -0600

    Migrate Hieu_so field. Fix reflection side Table View

--- DataAccess/Migrations/20220307225827_ds_cap_hieu_so.Designer.cs
+++ DataAccess/Migrations/20220307225827_ds_cap_hieu_so.Designer.cs
@@ -4,14 +4,16 @@ using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(TennisContext))]
-    partial class TennisContextModelSnapshot : ModelSnapshot
+    [Migration("20220307225827_ds_cap_hieu_so")]
+    partial class ds_cap_hieu_so
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
