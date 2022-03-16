0427105f0c5ea9588baa922a00a3285bc8f50059
Veronica Vu <ptv0002@uah.edu>
2022-03-16 03:07:07 -0500

    Update Thong bao, Login pics. Fix announcement forms

--- DataAccess/Migrations/20220316043515_up_thong_bao.Designer.cs
+++ DataAccess/Migrations/20220316043515_up_thong_bao.Designer.cs
@@ -4,14 +4,16 @@ using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(TennisContext))]
-    partial class TennisContextModelSnapshot : ModelSnapshot
+    [Migration("20220316043515_up_thong_bao")]
+    partial class up_thong_bao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/DS_Cap.cs
+++ Models/DS_Cap.cs
@@ -27,21 +27,17 @@
         public int Xep_Hang { get; set; }
         public int ID_Trinh { get; set; }
         [ForeignKey("ID_Trinh")]
-        [DisplayName("ID Trình")]
         public virtual DS_Trinh DS_Trinh { get; set; }
         public int? ID_Bang { get; set; }
         [ForeignKey("ID_Bang")]
-        [DisplayName("ID Bảng")]
         public virtual DS_Bang DS_Bang { get; set; }
         public int ID_Vdv1 { get; set; }
         [ForeignKey("ID_Vdv1")]
-        [DisplayName("ID VDV1")]
         public virtual DS_VDV VDV1 { get; set; }
 
         public int? ID_Vdv2 { get; set; }
         [ForeignKey("ID_Vdv2")]
         [InverseProperty("DS_Caps")]
-        [DisplayName("ID VDV2")]
         public virtual DS_VDV VDV2 { get; set; }
 
         public virtual ICollection<DS_Tran> DS_Trans { get; set; }
