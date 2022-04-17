4e7c90d733c41bdfa272b242898637d7f0780cd6
Veronica Vu <ptv0002@uah.edu>
2022-04-17 00:41:53 -0500

    Update index Norole page and migration

--- DataAccess/Migrations/20220415052805_edit_multiple_tables.Designer.cs
+++ DataAccess/Migrations/20220415052805_edit_multiple_tables.Designer.cs
@@ -4,14 +4,16 @@ using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(TennisContext))]
-    partial class TennisContextModelSnapshot : ModelSnapshot
+    [Migration("20220415052805_edit_multiple_tables")]
+    partial class edit_multiple_tables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
